ba2f7b56c6064249843a8a3da546fb4799e9ebb6
shudarshan khanal <shudarshank789@gmail.com>
2019-01-21 05:55:00 -0500

    editted file

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Hello World</h1>
+       <h1>Hello World .I am from Nepal</h1>
       </div>
     );
   }
